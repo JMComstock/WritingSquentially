d74805c0e5f0ebbeb4a9ac1159d8ef8adcb5fcc3
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-09-16 19:25:55 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,110 @@
+package com.company;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+             FileChannel binChannel = binFile.getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(100);
+
+//	        byte[] outputBytes = "Hello World!".getBytes();
+//	        byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
+//            buffer.flip();
+
+//            read(ByteBuffer) - reads bytes beginning at the channel's current position, and after the read,
+//                              updates the position accordingly.
+//                              Note that now we're talking about the channel's position, not the byte buffer's position.
+//            write(ByteBuffer) - the same as read, except it writes. There's one exception.
+//                              If a datasource is opened in APPEND mode, then the first write will take place stating
+//                              at the end of the datasource, rather than at position.  After the write, the position
+//                              will be updated accordingly.
+//            position() - returns the channel's position.
+//            position(long) - sets the channel's position to the passed value.
+//            truncate(long) - truncates the size of the attached datasource to the passed value.
+//            size() - returns the size of the attached datasource
+
+            byte[] outputBytes = "Hello World!".getBytes();
+            buffer.put(outputBytes);
+            long int1Pos = outputBytes.length;
+            buffer.putInt(245);
+            long int2Pos = int1Pos + Integer.BYTES;
+            buffer.putInt(-98765);
+            byte[] outputBytes2 = "Nice to meet you".getBytes();
+            buffer.put(outputBytes2);
+            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
+            buffer.putInt(1000);
+            buffer.flip();
+
+            binChannel.write(buffer);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
+            channel.position(int3Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+
+            System.out.println("int3 = " + readBuffer.getInt());
+            readBuffer.flip();
+            channel.position(int2Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+
+            System.out.println("in2 = " + readBuffer.getInt());
+            readBuffer.flip();
+            channel.position(int1Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+
+            System.out.println("int1 = " + readBuffer.getInt());
+
+            byte[] outputString = "Hello, World!".getBytes();
+            long str1Pos = 0;
+            long newInt1Pos = outputString.length;
+            long newInt2Pos = newInt1Pos + Integer.BYTES;
+            byte[] outputString2 = "Nice to meet you".getBytes();
+            long str2Pos = newInt2Pos + Integer.BYTES;
+            long newInt3Pos = str2Pos + outputString2.length;
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            binChannel.position(newInt1Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            binChannel.position(newInt2Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(1000);
+            intBuffer.flip();
+            binChannel.position(newInt3Pos);
+            binChannel.write(intBuffer);
+
+            binChannel.position(str1Pos);
+            binChannel.write(ByteBuffer.wrap(outputString));
+            binChannel.position(str2Pos);
+            binChannel.write(ByteBuffer.wrap(outputString2));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
